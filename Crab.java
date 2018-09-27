c814e8fa44b04f34c747674538d84fd19640beaf
ayserz119 <ayserz119@hotmail.com>
2018-09-27 11:00:33 -0500

    Add files via upload

---
+++ Crab.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Crab here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Crab extends Actor
+{
+    /**
+     * Act - do whatever the Crab wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move(4);
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(3);
+        }
+        
+         if(Greenfoot.isKeyDown("left"))
+        {
+            turn(-3);
+        }
+        
+        if (isTouching(Gusano.class))
+        {
+            removeTouching(Gusano.class);
+        }
+    }    
+    
+}
